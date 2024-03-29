98ca474ada86f65ff15eaa7ea981bda9893ec626
Kelyton de Lucas <kelytonlucas@gmail.com>
2024-03-28 22:29:31 -0300

    Basic code for mqtt comunication(java)

---
+++ demo/src/main/java/com/example/Main.java
@@ -0,0 +1,62 @@
+package com.example;
+
+import org.eclipse.paho.client.mqttv3.*;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        String broker = "tcp://192.168.1.50:1883";
+        String clientId = "";
+        MemoryPersistence persistence = new MemoryPersistence();
+
+        try {
+            MqttClient mqttClient = new MqttClient(broker, clientId, persistence);
+            mqttClient.connect();
+            
+            // Subscreva aos tópicos relevantes aqui
+            mqttClient.subscribe("teste/l");
+            
+            mqttClient.setCallback(new MqttCallback() {
+                public void connectionLost(Throwable throwable) {
+                }
+
+                public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+                    String message = new String(mqttMessage.getPayload());
+                    System.out.println("Mensagem recebida: " + message);
+                    
+                    //inserirNoBancoDeDados(message);
+                }
+
+                public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+                }
+            });
+            
+            mqttClient.close();
+
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /*private static void inserirNoBancoDeDados(String message) {
+        // Código para inserir os dados no banco de dados aqui
+    }*/
+
+}
+//----------------
+/*public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+    String message = new String(mqttMessage.getPayload());
+    System.out.println("Mensagem recebida: " + message);
+    
+    if (topic.equals("sensores/temperatura")) {
+        // Lógica para lidar com dados de temperatura
+        processarDadosTemperatura(message);
+    } else if (topic.equals("sensores/umidade")) {
+        // Lógica para lidar com dados de umidade
+        processarDadosUmidade(message);
+    } else {
+        // Lógica para outros tópicos
+        processarOutrosDados(message);
+    }
+}*/
